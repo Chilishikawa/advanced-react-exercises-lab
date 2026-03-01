d28a46d4a9ea6785784fbaee0e8846f6607e95e8
Chilishikawa <chilishikawa13@gmail.com>
2026-03-01 12:43:01 -0300

    actualización: se suben tareas

---
+++ light-dark-theme-switcher/src/ThemeContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext(undefined);
+
+export const ThemeProvider = ({ children }) => {
+
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(prevTheme =>
+            prevTheme === 'light' ? 'dark' : 'light'
+        );
+    };
+
+    const value = {
+        theme,
+        toggleTheme
+    }
+
+    return (
+        <ThemeContext.Provider value={ value }>{ children }</ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
